08183ffaa0d8964b492e82af77054cf64090725e
Elton Lac <lacelton@yahoo.com>
2021-02-22 17:50:15 -0800

    Added page template and style sheet

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const inquirer = require("inquirer");
 const path = require("path");
+const fs = require("fs");
 
 const teamMembers = [];
+const render = require("./src/page-template.js");
 
 function app() {
   function chooseEmployee() {
@@ -16,7 +18,8 @@ function app() {
           message: "Which employee are you adding?",
           choices: ["Manager", "Engineer", "Intern", "No more team members"],
         },
-      ]).then((userChoice) => {
+      ])
+      .then((userChoice) => {
         switch (userChoice.chooseEmployee) {
           case "Manager":
             addManager();
@@ -34,101 +37,126 @@ function app() {
   }
 
   function addManager() {
-    inquirer.prompt([
-      {
-        type: "input",
-        name: "managerName",
-        message: "What is the manager's name?",
-      },
-      {
-        type: "input",
-        name: "managerId",
-        message: "What is the manager's id?",
-      },
-      {
-        type: "input",
-        name: "managerEmail",
-        message: "What is the manager's email?",
-      },
-      {
-        type: "input",
-        name: "managerOffice",
-        message: "What is the manager's office number?",
-      },
-    ]).then(answers => {
-      const manager = new Manager(answers.mangerName, answers.managerId, answers.managerEmail, answers.managerOffice);
-      teamMembers.push(manager);
-      chooseEmployee();
-    })
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "managerName",
+          message: "What is the manager's name?",
+        },
+        {
+          type: "input",
+          name: "managerId",
+          message: "What is the manager's id?",
+        },
+        {
+          type: "input",
+          name: "managerEmail",
+          message: "What is the manager's email?",
+        },
+        {
+          type: "input",
+          name: "managerOffice",
+          message: "What is the manager's office number?",
+        },
+      ])
+      .then((answers) => {
+        const manager = new Manager(
+          answers.managerName,
+          answers.managerId,
+          answers.managerEmail,
+          answers.managerOffice
+        );
+        teamMembers.push(manager);
+        chooseEmployee();
+      });
   }
 
   function addEngineer() {
-    inquirer.prompt([
-      {
-        type: "input",
-        name: "engineerName",
-        message: "What is the engineer's name?",
-      },
-      {
-        type: "input",
-        name: "engineerId",
-        message: "What is the engineer's id?",
-      },
-      {
-        type: "input",
-        name: "engineerEmail",
-        message: "What is the engineer's email?",
-      },
-      {
-        type: "input",
-        name: "engineerGithub",
-        message: "What is the engineer's Github?",
-      },
-    ]).then(answers => {
-      const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
-      teamMembers.push(engineer);
-      chooseEmployee();
-    })
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "engineerName",
+          message: "What is the engineer's name?",
+        },
+        {
+          type: "input",
+          name: "engineerId",
+          message: "What is the engineer's id?",
+        },
+        {
+          type: "input",
+          name: "engineerEmail",
+          message: "What is the engineer's email?",
+        },
+        {
+          type: "input",
+          name: "engineerGithub",
+          message: "What is the engineer's Github?",
+        },
+      ])
+      .then((answers) => {
+        const engineer = new Engineer(
+          answers.engineerName,
+          answers.engineerId,
+          answers.engineerEmail,
+          answers.engineerGithub
+        );
+        teamMembers.push(engineer);
+        chooseEmployee();
+      });
   }
 
   function addIntern() {
-    inquirer.prompt([
-      {
-        type: "input",
-        name: "internName",
-        message: "What is the intern's name?",
-      },
-      {
-        type: "input",
-        name: "internId",
-        message: "What is the intern's id?",
-      },
-      {
-        type: "input",
-        name: "internEmail",
-        message: "What is the intern's email?",
-      },
-      {
-        type: "input",
-        name: "internSchool",
-        message: "Where school did the intern study at?",
-      },
-    ]).then(answers => {
-      const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
-      teamMembers.push(intern);
-      chooseEmployee();
-    })
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "internName",
+          message: "What is the intern's name?",
+        },
+        {
+          type: "input",
+          name: "internId",
+          message: "What is the intern's id?",
+        },
+        {
+          type: "input",
+          name: "internEmail",
+          message: "What is the intern's email?",
+        },
+        {
+          type: "input",
+          name: "internSchool",
+          message: "Where school did the intern study at?",
+        },
+      ])
+      .then((answers) => {
+        const intern = new Intern(
+          answers.internName,
+          answers.internId,
+          answers.internEmail,
+          answers.internSchool
+        );
+        teamMembers.push(intern);
+        chooseEmployee();
+      });
   }
 
   function combine() {
     if (!fs.existsSync(path.resolve(__dirname, "dist"))) {
-      fs.mkdirSync(path.resolve(__dirname, "dist"))
+      fs.mkdirSync(path.resolve(__dirname, "dist"));
     }
-    fs.writeFileSync(path.join(path.resolve(__dirname, "dist"), "employees.html"), render(teamMembers), "utf-8");
+    fs.writeFileSync(
+      path.join(path.resolve(__dirname, "dist"), "employees.html"),
+      render(teamMembers),
+      "utf-8"
+    );
+    console.log("Creating team!")
   }
 
   chooseEmployee();
-
 }
 
 app();
